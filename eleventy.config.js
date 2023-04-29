6c309d06d36be3d3503b2161eefc5fbe925ec4b8
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-04-29 12:14:19 +0200

    fix(base.njk): update css include path from "public/css/index.css" to "assets/css/index.css"
    feat(index.css): add default theme colors and global stylesheet
    feat(index.css): add media query for dark mode
    feat(index.css): add styles for header, nav, posts list, tags, and direct links/markdown headers
    
    refactor(eleventy.config.js): change public folder to assets folder
    refactor(eleventy.config.js): change content folder to src folder

--- eleventy.config.js
+++ eleventy.config.js
@@ -12,9 +12,9 @@ const pluginImages = require("./eleventy.config.images.js");
 
 module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
-	// For example, `./public/css/` ends up in `_site/css/`
+	// For example, `./assets/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		"./public/": "/",
+		"./assets/": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
 	});
 
@@ -22,7 +22,7 @@ module.exports = function(eleventyConfig) {
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
 	// Watch content images for the image pipeline.
-	eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpeg}");
+	eleventyConfig.addWatchTarget("src/**/*.{svg,webp,png,jpeg}");
 
 	// App plugins
 	eleventyConfig.addPlugin(pluginDrafts);
@@ -118,7 +118,7 @@ module.exports = function(eleventyConfig) {
 
 		// These are all optional:
 		dir: {
-			input: "content",          // default: "."
+			input: "src",          // default: "."
 			includes: "../_includes",  // default: "_includes"
 			data: "../_data",          // default: "_data"
 			output: "_site"

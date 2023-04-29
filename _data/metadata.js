8ad5b3298e642a1968f3becb2aecfa82985e0bdf
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-04-29 11:59:36 +0200

    feat(metadata.js): add metadata for Monero website in German language

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Monero",
+	url: "https://example.com/",
+	language: "de",
+	description: "Die offizielle Website der deutschsprachigen Monero-Gemeinschaft",
+	author: {
+		name: "Die deutschsprachige Monero-Gemeinschaft",
+		email: "as@wm3.ch",
+		url: "https://example.com/"
+	}
+}

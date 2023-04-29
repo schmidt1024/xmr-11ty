b79880d064bdb2594487541c9d67c2881d48b43c
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-04-29 17:49:42 +0200

    chore(metadata.js, sitemap.xml): update website URL to https://aschmidt1024.github.io/ and fix indentation

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Monero",
-	url: "https://example.com/",
+	url: "https://aschmidt1024.github.io/xmr-11ty/_site/",
 	language: "de",
 	description: "Die offizielle Website der deutschsprachigen Monero-Gemeinschaft",
 	author: {
 		name: "Die deutschsprachige Monero-Gemeinschaft",
 		email: "as@wm3.ch",
-		url: "https://example.com/"
+		url: "https://aschmidt1024.github.io/xmr-11ty/_site/"
 	}
 }
